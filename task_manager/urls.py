6b58548d58f8578a4b1ace0230816c047d7029db
Sergey <sergeybirich@gmail.com>
2017-03-12 17:35:33 +0300

    Added urls

---
+++ task_manager/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from task_manager import views
+
+
+router = DefaultRouter()
+router.register(r'projects', views.ProjectViewSet)
+router.register(r'tasks', views.TaskViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
