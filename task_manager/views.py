aae967b49f2d1077fe611023a981645b48d4e463
Sergey <sergeybirich@gmail.com>
2017-03-12 18:12:25 +0300

    Added permissions

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from task_manager.models import Person, Project, Task
+from task_manager.permissions import IsManagerOrReadOnly
+from task_manager.serializers import ProjectSerializer, TaskSerializer, UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsManagerOrReadOnly,)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsManagerOrReadOnly,)
+
