d964d12d8e55a8d0ebb3a515dc043585c85096f6
Sergey <sergeybirich@gmail.com>
2017-03-14 13:16:42 +0300

    Added get_absolute_url to models

--- task_manager/models.py
+++ task_manager/models.py
@@ -2,21 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Role(models.Model):
-    MANAGER = 'MG'
-    DEVELOPER = 'DEV'
+class User(AbstractUser):
+    MANAGER = 0
+    DEVELOPER = 1
     ROLES = (
         (MANAGER, 'Manager'),
         (DEVELOPER, 'Developer'),
     )
-    description = models.CharField(max_length=50, null=False, choices=ROLES,
-                                   blank=False, unique=True)
+    role = models.CharField(max_length=10,null=False, blank=False,
+                            choices=ROLES, default=MANAGER)
 
+    def is_manager(self):
+        return self.role == 0
 
-class User(AbstractUser):
-    DEFAULT_ROLE_ID = 1
-    role = models.ForeignKey(Role, related_name='users', null=False,
-                             blank=False, default=DEFAULT_ROLE_ID)  # ON_DELETE
+    def get_absolute_url(self):
+        return ('developers/{}' if self.role == User.DEVELOPER else 'managers/{}').format(self.id)
 
 
 class CommonInfo(models.Model):
@@ -28,15 +28,27 @@ class CommonInfo(models.Model):
         abstract = True
         ordering = ('due_date',)
 
+    def __str__(self):
+        return self.title
+
 
 class Task(CommonInfo):
     project = models.ForeignKey('Project', related_name='tasks',
                                 on_delete=models.CASCADE)
     executor = models.ForeignKey(User, related_name='tasks',
                                  on_delete=models.PROTECT)
+    reporter = models.ForeignKey(User, related_name='controlled_tasks',
+                                 on_delete=models.PROTECT)
     is_finished = models.BooleanField()
+
+    def get_absolute_url(self):
+        return 'tasks/{}'.format(self.id)
 
 
 class Project(CommonInfo):
     team = models.ManyToManyField(User, related_name='projects')
+
+    def get_absolute_url(self):
+        return 'projects/{}'.format(self.id)
+
 
